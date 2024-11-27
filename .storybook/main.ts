1cab0815d56a07c38ca1a053e0fc860cbfc0c6cc
takahashiyuzuki <taka.yuzu1127@gmail.com>
2024-11-27 16:45:08 +0900

    nx.js関係を削除、pnpmでStorybookを起動できるように修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,22 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
+  ],
+  
+
   framework: {
-    name: '@storybook/nextjs',
-    options: {
-      builder: {
-        viteConfigPath: 'vite.config.ts'
-      }
-    }
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
   }
 }
 
